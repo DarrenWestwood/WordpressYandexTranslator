ebc7c7c23e97813f465ed153f3b1a569f23bfe92
Darren <39666372+DarrenWestwood@users.noreply.github.com>
2018-08-23 02:22:28 +0200

    Create auto-translate-yandex.php
    Main plugin file creation

---
+++ auto-translate-yandex/auto-translate-yandex.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Auto_Translate_Yandex
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Auto Translate Yandex
+ * Plugin URI:        http://example.com/auto-translate-yandex/
+ * Description:       Automatically translate your posts using the Yandex Translate API.
+ * Version:           1.0.0
+ * Author:            Darren Westwood
+ * Author URI:        http://darrenwestwood.tk/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       auto-translate-yandex
+ * Domain Path:       /languages
+ */
+?>
